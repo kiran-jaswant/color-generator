81af2d5337f78da903373a0daf368e4c8250e425
kiran-jaswant <kiranjaswant08@gmail.com>
2025-04-06 15:10:46 +0530

    color generator created

---
+++ app.js
@@ -0,0 +1,14 @@
+let btn= document.querySelector("button");
+let head=document.querySelector("h1");
+let box=document.querySelector("div");
+
+btn.addEventListener("click",function(){
+
+    let red= Math.floor(Math.random()*255);
+    let green= Math.floor(Math.random()*255);
+    let blue= Math.floor(Math.random()*255);
+    
+    head.innerText=(`rgb(${red},${green},${blue})`);
+    box.style.backgroundColor=`rgb(${red},${green},${blue})`
+
+})
